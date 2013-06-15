7d27d607f7152fdc40bcf416d4fb69dcc617ec5a
Zhou Xinzi <imdreamrunner@gmail.com>
2013-06-15 23:00:28 +0800

    Add path display in LLKan

---
+++ examples/LLKan/scripts/calculate.js
@@ -0,0 +1,282 @@
+function isMatch(a, b) {
+  if (a.get('type') !== b.get('type')) {
+    return false;
+  }
+  var layer = a.layer;
+  function hasBrick(x, y) {
+    var bricks = layer.where({
+      left: x,
+      top: y,
+      exploded: false
+    });
+    return !!bricks.length;
+  }
+  var ax = a.get('left');
+  var ay = a.get('top');
+  var bx = b.get('left');
+  var by = b.get('top');
+  var t;
+  if (ax > bx) {
+    t = bx;
+    bx = ax;
+    ax = t;
+    t = by;
+    by = ay;
+    ay = t;
+  }
+
+  function findMax(startX, startY, direction) {
+    var vertical = direction === 0 || direction === 2;
+    var step = (direction === 1 || direction === 2) ? 1 : -1;
+    var target = vertical ? startY : startX;
+    while (true) {
+      target += step;
+      if (target < 0) {
+        return -1;
+      }
+      if (vertical) {
+        if (target > (HEIGHT - 1)) {
+          return HEIGHT
+        }
+        if (hasBrick(startX, target)) {
+          return target - step;
+        }
+      } else {
+        if (target > (WIDTH - 1)) {
+          return WIDTH;
+        }
+        if (hasBrick(target, startY)) {
+          return target - step;
+        }
+      }
+    }
+  }
+
+  // Z style
+
+  var ax_min = findMax(ax, ay, 3),
+    ax_max = findMax(ax, ay, 1),
+    bx_min = findMax(bx, by, 3),
+    bx_max = findMax(bx, by, 1);
+
+  var left = Math.max(ax_min, bx_min),
+    right = Math.min(ax_max, bx_max);
+  if (left <= right) {
+    for (var tx = left; tx <= right; tx++) {
+      if (ay < by) {
+        if (findMax(tx, ay, 2) >= (by - 1)) {
+          return true;
+        }
+      } else {
+        if (findMax(tx, ay, 0) <= (by + 1)) {
+          return true;
+        }
+      }
+    }
+  }
+
+  // S style
+  var ay_min = findMax(ax, ay, 0),
+    ay_max = findMax(ax, ay, 2),
+    by_min = findMax(bx, by, 0),
+    by_max = findMax(bx, by, 2);
+
+  var top = Math.max(ay_min, by_min),
+    bottom = Math.min(ay_max, by_max);
+  if (top <= bottom) {
+    for (var ty = top; ty <= bottom; ty++) {
+      if (ax < bx) {
+        if (findMax(ax, ty, 1) >= (bx - 1)) {
+          return true;
+        }
+      } else {
+        if (findMax(ax, ty, 3) <= (bx + 1)) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+}
+
+function findPath(a, b) {
+  var layer = a.layer;
+  function hasBrick(x, y) {
+    var bricks = layer.where({
+      left: x,
+      top: y,
+      exploded: false
+    });
+    return !!bricks.length;
+  }
+  var ax = a.get('left');
+  var ay = a.get('top');
+  var bx = b.get('left');
+  var by = b.get('top');
+  var t;
+  if (ax > bx) {
+    t = bx;
+    bx = ax;
+    ax = t;
+    t = by;
+    by = ay;
+    ay = t;
+  }
+
+  function findMax(startX, startY, direction) {
+    var vertical = direction === 0 || direction === 2;
+    var step = (direction === 1 || direction === 2) ? 1 : -1;
+    var target = vertical ? startY : startX;
+    while (true) {
+      target += step;
+      if (target < 0) {
+        return -1;
+      }
+      if (vertical) {
+        if (target > (HEIGHT - 1)) {
+          return HEIGHT
+        }
+        if (hasBrick(startX, target)) {
+          return target - step;
+        }
+      } else {
+        if (target > (WIDTH - 1)) {
+          return WIDTH;
+        }
+        if (hasBrick(target, startY)) {
+          return target - step;
+        }
+      }
+    }
+  }
+
+  // Z style
+
+  var ax_min = findMax(ax, ay, 3),
+    ax_max = findMax(ax, ay, 1),
+    bx_min = findMax(bx, by, 3),
+    bx_max = findMax(bx, by, 1);
+
+  var left = Math.max(ax_min, bx_min),
+    right = Math.min(ax_max, bx_max);
+  if (left <= right) {
+    for (var tx = left; tx <= right; tx++) {
+      if (ay < by) {
+        if (findMax(tx, ay, 2) >= (by - 1)) {
+          return {
+            type: 0,
+            x: tx
+          };
+        }
+      } else {
+        if (findMax(tx, ay, 0) <= (by + 1)) {
+          return {
+            type: 0,
+            x: tx
+          };
+        }
+      }
+    }
+  }
+
+  // S style
+  var ay_min = findMax(ax, ay, 0),
+    ay_max = findMax(ax, ay, 2),
+    by_min = findMax(bx, by, 0),
+    by_max = findMax(bx, by, 2);
+
+  var top = Math.max(ay_min, by_min),
+    bottom = Math.min(ay_max, by_max);
+  if (top <= bottom) {
+    for (var ty = top; ty <= bottom; ty++) {
+      if (ax < bx) {
+        if (findMax(ax, ty, 1) >= (bx - 1)) {
+          return {
+            type: 1,
+            y: ty
+          };
+        }
+      } else {
+        if (findMax(ax, ty, 3) <= (bx + 1)) {
+          return {
+            type: 1,
+            y: ty
+          };
+        }
+      }
+    }
+  }
+
+  return false;
+}
+
+
+function drawFire(a, b) {
+  var layer = a.layer;
+  var ax = a.get('left');
+  var ay = a.get('top');
+  var bx = b.get('left');
+  var by = b.get('top');
+  var stepX, stepY;
+  var path = findPath(a, b);
+  if (path.type === 0) {
+    stepX = ax < path.x ? 1 : -1;
+    for (var i = ax; i !== path.x; i += stepX) {
+      fireLayer.attach(new Fire({
+        top: ay,
+        left: i
+      }));
+    }
+    stepY = ay < by ? 1 : -1;
+    for (var i = ay; i !== by; i += stepY) {
+      fireLayer.attach(new Fire({
+        top: i,
+        left: path.x
+      }));
+    }
+    stepX = path.x < bx ? 1 : -1;
+    for (var i = path.x; i !== bx; i += stepX) {
+      fireLayer.attach(new Fire({
+        top: by,
+        left: i
+      }));
+    }
+    fireLayer.attach(new Fire({
+      top: by,
+      left: bx
+    }));
+  } else {
+    stepY = ay < path.y ? 1 : -1;
+    for (var i = ay; i !== path.y; i += stepY) {
+      fireLayer.attach(new Fire({
+        top: i,
+        left: ax
+      }));
+    }
+    stepX = ax < bx ? 1 : -1;
+    for (var i = ax; i !== bx; i += stepX) {
+      fireLayer.attach(new Fire({
+        top: path.y,
+        left: i
+      }));
+    }
+    stepY = path.y < by ? 1 : -1;
+    for (var i = path.y; i !== by; i += stepY) {
+      fireLayer.attach(new Fire({
+        top: i,
+        left: bx
+      }));
+    }
+    fireLayer.attach(new Fire({
+      top: by,
+      left: bx
+    }));
+  }
+  fireLayer.eachChild(function(sprite) {
+    sprite.runAction(new Kaya.Action.Queue([
+      new Kaya.Action.FadeOut(1000),
+      new Kaya.Action.Remove()
+    ]));
+  });
+}
--- examples/LLKan/scripts/game.js
+++ examples/LLKan/scripts/game.js
@@ -18,119 +18,26 @@ var BrickColor = [
   '#F0F'
 ];
 
-function isMatch(a, b) {
-  if (a.get('type') !== b.get('type')) {
-    return false;
-  }
-  var layer = a.layer;
-  function hasBrick(x, y) {
-    var bricks = layer.where({
-      left: x,
-      top: y,
-      exploded: false
+
+var Fire = Kaya.Sprite.Rectangular.extend({
+  dataTypes: Kaya.Utilities.extend(Kaya.Sprite.Rectangular.prototype.dataTypes, {
+    top: 'Integer',
+    left: 'Integer',
+    type: 'Integer'
+  }),
+
+  constructor: function() {
+    this._super.apply(this, arguments);
+    this.set({
+      'width': 40,
+      'height': 40,
+      'x': 100 + this.get('left') * 40,
+      'y': 100 + this.get('top') * 40,
+      'color': '#FFF',
+      alpha: 0.8
     });
-    return !!bricks.length;
   }
-  var ax = a.get('left');
-  var ay = a.get('top');
-  var bx = b.get('left');
-  var by = b.get('top');
-  var t;
-  if (ax > bx) {
-    t = bx;
-    bx = ax;
-    ax = t;
-    t = by;
-    by = ay;
-    ay = t;
-  }
-
-  function findMax(startX, startY, direction) {
-    var vertical = direction === 0 || direction === 2;
-    var step = (direction === 1 || direction === 2) ? 1 : -1;
-    var target = vertical ? startY : startX;
-    while (true) {
-      target += step;
-      if (target < 0) {
-        return -1;
-      }
-      if (vertical) {
-        if (target > (HEIGHT - 1)) {
-          return HEIGHT
-        }
-        if (hasBrick(startX, target)) {
-          return target - step;
-        }
-      } else {
-        if (target > (WIDTH - 1)) {
-          return WIDTH;
-        }
-        if (hasBrick(target, startY)) {
-          return target - step;
-        }
-      }
-    }
-  }
-
-  // Z style
-
-  var ax_min = findMax(ax, ay, 3),
-    ax_max = findMax(ax, ay, 1),
-    bx_min = findMax(bx, by, 3),
-    bx_max = findMax(bx, by, 1);
-
-  var left = Math.max(ax_min, bx_min),
-    right = Math.min(ax_max, bx_max);
-  if (left <= right) {
-    if (Math.abs(ay - by) === 1) {
-      console.log('A');
-      return true;
-    }
-    for (var tx = left; tx <= right; tx++) {
-      if (ay < by) {
-        if (findMax(tx, ay, 2) >= (by - 1)) {
-          console.log('B');
-          return true;
-        }
-      } else {
-        if (findMax(tx, ay, 0) <= (by + 1)) {
-          console.log('C');
-          return true;
-        }
-      }
-    }
-  }
-
-  // S style
-  var ay_min = findMax(ax, ay, 0),
-    ay_max = findMax(ax, ay, 2),
-    by_min = findMax(bx, by, 0),
-    by_max = findMax(bx, by, 2);
-
-  var top = Math.max(ay_min, by_min),
-    bottom = Math.min(ay_max, by_max);
-  if (top <= bottom) {
-    if (Math.abs(ax - bx) === 1) {
-      console.log('D');
-      return true;
-    }
-    for (var ty = top; ty <= bottom; ty++) {
-      if (ax < bx) {
-        if (findMax(ax, ty, 1) >= (bx - 1)) {
-          console.log('E');
-          return true;
-        }
-      } else {
-        if (findMax(ax, ty, 3) <= (bx + 1)) {
-          console.log('F');
-          return true;
-        }
-      }
-    }
-  }
-
-  return false;
-}
+});
 
 var Brick = Kaya.Sprite.Rectangular.extend({
   dataTypes: Kaya.Utilities.extend(Kaya.Sprite.Rectangular.prototype.dataTypes, {
@@ -183,6 +90,7 @@ var Brick = Kaya.Sprite.Rectangular.extend({
     if (selectedList.length === 1) {
       var target = selectedList[0];
       if (isMatch(this, target)) {
+        drawFire(this, target);
         this.explode();
         target.explode();
       } else {
@@ -204,6 +112,9 @@ var Brick = Kaya.Sprite.Rectangular.extend({
     }
   }
 });
+
+var brickLayer = new Kaya.Layer();
+var fireLayer = new Kaya.Layer();
 
 var GameStage = Kaya.Stage.extend({
   initialize: function() {
@@ -229,8 +140,8 @@ var GameStage = Kaya.Stage.extend({
       });
       return brick;
     }
-    var brickLayer = new Kaya.Layer();
     this.attach(brickLayer);
+    this.attach(fireLayer);
     var brickTypes = [];
     var total = 0;
     for (var type in LEVEL) {

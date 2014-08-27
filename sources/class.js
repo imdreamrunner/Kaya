1b06c0e9effc545d394592051f1eb9533a3ed893
Zhou Xinzi <imdreamrunner@gmail.com>
2014-08-27 11:27:18 +0800

    add base classes

---
+++ sources/class.js
@@ -0,0 +1,66 @@
+/*
+ * Class
+ * Based on Simple JavaScript Inheritance
+ * http://ejohn.org/blog/simple-javascript-inheritance/
+ *
+ * Modified to use Object.clone to replace new for inheritance.
+ */
+
+Kaya.Class = function() {};
+
+// Return a sub class.
+Kaya.Class.extend = function(prop) {
+  var fnTest = /\b_super\b/;
+  var _super = this.prototype;
+
+  // Instantiate a base class (but only create the instance,
+  // don't run the init constructor
+  var prototype = Object.create(this.prototype);
+
+  // Copy the properties over onto the new prototype
+  for (var name in prop) {
+    // Check if we're overwriting an existing function
+    if (prop.hasOwnProperty(name)) {
+      if (typeof prop[name] === "function") {
+        prototype[name] = (function (name, fn) {
+          return function () {
+            // Add a new ._super() method that is the same method
+            // but on the super-class
+            this._super = _super[name];
+
+            // The method only need to be bound temporarily, so we
+            // remove it when we're done executing
+            var ret = fn.apply(this, arguments);
+
+            delete this['_super'];
+
+            return ret;
+          };
+        })(name, prop[name]);
+      } else {
+        prototype[name] = prop[name];
+      }
+    }
+  }
+
+  // The dummy class constructor
+  function Class() {
+    // Generate an unique id for every class.
+    this._id = Kaya.uniqueId();
+
+    if (this.constructor) {
+      this.constructor.apply(this, arguments);
+    }
+  }
+
+  // Populate our constructed prototype object
+  Class.prototype = prototype;
+
+  // Enforce the constructor to be what we expect
+  Class.constructor = Class;
+
+  Class.extend = Kaya.Class.extend;
+  Class.join = Kaya.Class.join;
+
+  return Class;
+};
---
+++ sources/utilities.js
@@ -0,0 +1,138 @@
+/*
+ * Utilities use by Kaya framework.
+ */
+
+Kaya.Utilities = {
+  extend: function(target) {
+    var sources = Array.prototype.slice.call(arguments, 1);
+    sources.forEach(function(source){
+      if (source) {
+        for (var name in source) {
+          target[name] = source[name];
+        }
+      }
+    });
+    return target;
+  },
+
+  clone: function(source) {
+    // Base on http://stackoverflow.com/questions/728360/most-elegant-way-to-clone-a-javascript-object
+
+    if (typeof source !== 'object') {
+      return source;
+    }
+
+    var target;
+
+    // Handle Date
+    if (source instanceof Date) {
+      target = new Date();
+      target.setTime(source.getTime());
+      return target;
+    }
+
+    // Handle Array
+    if (source instanceof Array) {
+      target = Array.prototype.slice.call(source);
+      return target;
+    }
+
+    // Handle Object
+    if (source instanceof Object) {
+      target = Kaya.Utilities.extend({}, source);
+      return target;
+    }
+
+    throw new Error("Failed to clone an object.");
+  },
+
+  hexToRgb: function(hex) {
+    // Code from http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  },
+
+  rgbToHex: function(r, g, b) {
+    function componentToHex(c) {
+      if (c > 255) {
+        c = 255;
+      }
+      var hex = c.toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+    }
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  },
+
+  bezier: function(x1, y1, x2, y2, epsilon){
+    /*
+     * from https://github.com/arian/cubic-bezier
+     */
+
+    var curveX = function(t){
+      var v = 1 - t;
+      return 3 * v * v * t * x1 + 3 * v * t * t * x2 + t * t * t;
+    };
+
+    var curveY = function(t){
+      var v = 1 - t;
+      return 3 * v * v * t * y1 + 3 * v * t * t * y2 + t * t * t;
+    };
+
+    var derivativeCurveX = function(t){
+      var v = 1 - t;
+      return 3 * (2 * (t - 1) * t + v * v) * x1 + 3 * (- t * t * t + 2 * v * t) * x2;
+    };
+
+    return function(t){
+
+      var x = t, t0, t1, t2, x2, d2, i;
+
+      // First try a few iterations of Newton's method -- normally very fast.
+      // Sometimes bugs appear when using this method, need to be fixed.
+      /*
+       for (t2 = x, i = 0; i < 8; i++){
+       x2 = curveX(t2) - x;
+       if (Math.abs(x2) < epsilon) return curveY(t2);
+       d2 = derivativeCurveX(t2);
+       if (Math.abs(d2) < 1e-6) break;
+       t2 = t2 - x2 / d2;
+       }
+       */
+
+      t0 = 0; t1 = 1; t2 = x;
+
+      if (t2 < t0) return curveY(t0);
+      if (t2 > t1) return curveY(t1);
+
+      // Fallback to the bisection method for reliability.
+      while (t0 < t1){
+        x2 = curveX(t2);
+        if (Math.abs(x2 - x) < epsilon) return curveY(t2);
+        if (x > x2) t0 = t2;
+        else t1 = t2;
+        t2 = (t1 - t0) * .5 + t0;
+      }
+
+      // Failure
+      return curveY(t2);
+
+    };
+
+  },
+
+  shuffle: function(o){
+    // http://jsfromhell.com/array/shuffle
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  }
+};
